faff9665c4e5aa13491ebe9bb0bc3df283fd2064
Maaaaark <s1251789436@163.com>
2023-10-25 13:17:51 +0800

    增加标注

--- main.m
+++ main.m
@@ -5,5 +5,8 @@ clf
 out=real(saleh(x));
 figure(1)
 plot(abs(x(:,1)),abs(out(:,1)));
+xlabel("input")
+ylabel("output")
+title("AM/AM")
 
 audiowrite('C:\Users\Administrator\Music\Cyberpunk 2077\I Really Want to Stay At Your House.flac',out,FS);
